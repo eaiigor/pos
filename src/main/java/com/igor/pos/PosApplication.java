597f584bcce51c7f426c0ccf8fd9de0e7f664189
Igor Rocha Lima <contatoigorrochalima@hotmail.com>
2023-11-07 15:08:39 -0300

    Criando operacao de instanciacao

--- src/main/java/com/igor/pos/PosApplication.java
+++ src/main/java/com/igor/pos/PosApplication.java
@@ -1,13 +1,31 @@
 package com.igor.pos;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class PosApplication {
+import com.igor.pos.domain.Categoria;
+import com.igor.pos.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class PosApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PosApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
